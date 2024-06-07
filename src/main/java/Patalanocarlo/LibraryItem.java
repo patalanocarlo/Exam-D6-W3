348edd7df3b28bf5fa7ce2b1efca3828e693dd27
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-06-07 09:19:26 +0200

    Creo la classe padre che poi andrò ad estendere ai figli

--- src/main/java/Patalanocarlo/LibraryItem.java
+++ src/main/java/Patalanocarlo/LibraryItem.java
@@ -9,4 +9,37 @@ import jakarta.persistence.InheritanceType;
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public class LibraryItem {
+    @id
+    private String isbn;
+    private String title;
+    private int publicationYear;
+    private int numberOfPages;
+
+
+    public String getIsbn(){
+        return isbn;
+    }
+    public void setIsbn(String isbn){
+        this.isbn=isbn;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public int getPublicationYear(){
+        return publicationYear;
+    }
+    public void setPublicationYear(int publicationYear){
+        this.publicationYear=publicationYear;
+    }
+    public int getNumberOfPages(){
+        return numberOfPages;
+    }
+    public void setNumberOfPages(int numberOfPages){
+        this.numberOfPages=numberOfPages;
+    }
+
+
 }
